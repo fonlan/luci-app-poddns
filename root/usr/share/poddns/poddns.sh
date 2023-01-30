d85c6745c15c4db489ac08dd10d58b657e89cc4b
fonlan <fonlan@gmail.com>
2023-01-30 07:31:19 +0200

    Change wan ip obtain method. Remove WAN device from UI.

--- root/usr/share/poddns/poddns.sh
+++ root/usr/share/poddns/poddns.sh
@@ -9,5 +9,5 @@ iface=$(uci -q get poddns.domain.iface)
 record=$(curl -4 -X POST https://dnsapi.cn/Record.List -d "login_token=$token&domain=$main_domain&sub_domain=$sub_domain&format=json" | jsonfilter -e "@.records[0]")
 record_id=$(echo $record | jsonfilter -e "@.id")
 line_id=$(echo $record | jsonfilter -e "@.line_id")
-local_ip=$(ip addr show dev $iface | grep "inet " | awk "{print $2}")
+local_ip=$(ifstatus wan | jsonfilter -e '@["ipv4-address"][0].address')
 curl -4 -X POST https://dnsapi.cn/Record.Ddns -d "login_token=$token&format=json&domain=$main_domain&record_id=$record_id&record_line_id=$line_id&sub_domain=$sub_domain&value=$local_ip" | jq | logger -t poddns
