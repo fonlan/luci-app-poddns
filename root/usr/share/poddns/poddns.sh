f200f38de96428c6541bae15f6f1f4951ad7a63e
fonlan <fonlan@gmail.com>
2023-01-31 22:56:38 +0800

    Update poddns.sh
    remove non-used iface

--- root/usr/share/poddns/poddns.sh
+++ root/usr/share/poddns/poddns.sh
@@ -5,7 +5,6 @@ token=$(uci -q get poddns.config.token)
 sub_domain=$(uci -q get poddns.domain.sub_domain)
 main_domain=$(uci -q get poddns.domain.main_domain)
 logger -t poddns "Start update DDNS for $sub_domain.$main_domain"
-iface=$(uci -q get poddns.domain.iface)
 record=$(curl -4 -X POST https://dnsapi.cn/Record.List -d "login_token=$token&domain=$main_domain&sub_domain=$sub_domain&format=json" | jsonfilter -e "@.records[0]")
 record_id=$(echo $record | jsonfilter -e "@.id")
 line_id=$(echo $record | jsonfilter -e "@.line_id")
