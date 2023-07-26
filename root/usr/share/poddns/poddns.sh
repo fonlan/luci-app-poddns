780d8f986793353789c095ff5eb1325a6d0905c1
fayang <fan-fonlan.yang@nokia-sbell.com>
2023-07-26 04:35:47 +0300

    Skip update DDNS when IP not change

--- root/usr/share/poddns/poddns.sh
+++ root/usr/share/poddns/poddns.sh
@@ -5,8 +5,11 @@ token=$(uci -q get poddns.config.token)
 sub_domain=$(uci -q get poddns.domain.sub_domain)
 main_domain=$(uci -q get poddns.domain.main_domain)
 logger -t poddns "Start update DDNS for $sub_domain.$main_domain"
-record=$(curl -4 -X POST https://dnsapi.cn/Record.List -d "login_token=$token&domain=$main_domain&sub_domain=$sub_domain&format=json" | jsonfilter -e "@.records[0]")
+record=$(curl -s -4 -X POST https://dnsapi.cn/Record.List -d "login_token=$token&domain=$main_domain&sub_domain=$sub_domain&format=json" | jsonfilter -e "@.records[0]")
 record_id=$(echo $record | jsonfilter -e "@.id")
 line_id=$(echo $record | jsonfilter -e "@.line_id")
-local_ip=$(ifstatus wan | jsonfilter -e '@["ipv4-address"][0].address')
-curl -4 -X POST https://dnsapi.cn/Record.Ddns -d "login_token=$token&format=json&domain=$main_domain&record_id=$record_id&record_line_id=$line_id&sub_domain=$sub_domain&value=$local_ip" | jq | logger -t poddns
+ddns_ip=$(echo $record | jsonfilter -e "@.value")
+wan_ip=$(ifstatus wan | jsonfilter -e '@["ipv4-address"][0].address')
+if [ "$wan_ip" != "$ddns_ip" ]; then
+    curl -4 -X POST https://dnsapi.cn/Record.Ddns -d "login_token=$token&format=json&domain=$main_domain&record_id=$record_id&record_line_id=$line_id&sub_domain=$sub_domain&value=$wan_ip" | jq | logger -t poddns
+fi
